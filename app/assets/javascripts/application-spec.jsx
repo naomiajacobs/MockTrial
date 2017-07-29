10b845d7d7a3713ef0bfb109a7e1bd9f604c6764
Naomi Jacobs <naomiajacobs@gmail.com>
2017-07-29 10:57:58 -0700

    Add support for Jest testing

--- app/assets/javascripts/application-spec.jsx
+++ app/assets/javascripts/application-spec.jsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import { shallow } from 'enzyme';
 import Application from './application';
 
 it('should do the thing', () => {
-
+  const wrapper = shallow(<Application />)
+  expect(wrapper).toIncludeText('Hello, World!')
 });
