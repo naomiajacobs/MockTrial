96358ba58af3db30d61dff92d38fbf9a6d37f908
Naomi Jacobs <naomiajacobs@gmail.com>
2017-07-29 03:47:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# mock_trial
+Mock Trial Tournament Runner
+
+Current status: boilerplate
+- Webpack, React are set up
+- no frontend testing working yet
+- no CI yet
+- no selenium yet
---
+++ app/assets/javascripts/application.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './application.scss';
+
+ReactDOM.render(
+  <div className="hello" >Hello, World!</div>,
+  document.getElementById('app')
+);
