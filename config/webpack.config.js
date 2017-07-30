6eae85c38e5954417143d681060809ddf72a3f53
Naomi Jacobs <naomiajacobs@gmail.com>
2017-07-29 20:23:09 -0700

    Fix webpack setup

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -9,7 +9,7 @@ const testing = process.env.NODE_ENV === 'test'
 
 let config = {
   entry: {
-    'application': './app/assets/javascripts/application.jsx',
+    'index': './app/assets/javascripts/index.jsx',
     'styles': './app/assets/javascripts/application.scss',
     'specs': './app/assets/javascripts/specs.js',
   },
