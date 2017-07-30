0f05e088d88aad25894a70cd51a83a9c5d45d5d9
Naomi Jacobs <naomiajacobs@gmail.com>
2017-07-29 21:23:32 -0700

    Add validations for user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  attr_accessor :name, :email
+
+  validates :username, presence: true, length: { maximum: 50 }
+  validates :email,
+    presence: true,
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
 end
