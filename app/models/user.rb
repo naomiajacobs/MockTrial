3baf618c47f8d79dbc15cd0c7f4fa3fd2236a153
Naomi Jacobs <naomiajacobs@gmail.com>
2017-07-30 10:42:00 -0700

    Add password validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  attr_accessor :name, :email
+  has_secure_password
+
+  attribute :name, :string
+  attribute :email, :string
 
   validates :username, presence: true, length: { maximum: 50 }
-  validates :email,
-    presence: true,
-    length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'is a valid model' do
-    user = User.new(username: 'naomiajacobs', email: 'naomiajacobs@gmail.com')
+    user = User.new(
+      username: 'naomiajacobs',
+      email: 'naomiajacobs@gmail.com',
+      password: 'buttfac',
+      password_confirmation: 'buttfac'
+    )
     expect(user).to be_valid
   end
 end
