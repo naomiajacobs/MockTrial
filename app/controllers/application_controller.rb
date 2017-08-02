425a445d3227f002dee9f507c8d3d521d8e2bac1
Naomi Jacobs <naomiajacobs@gmail.com>
2017-08-01 19:52:34 -0700

    Require login for all pages other than signup and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  before_action :set_logged_in
+  before_action :set_logged_in, :require_login
+  skip_before_action :require_login, only: [:new, :create]
 
   def new
   end
@@ -13,4 +14,7 @@ class ApplicationController < ActionController::Base
     @logged_in = logged_in?
   end
 
+  def require_login
+    redirect_to root_url unless logged_in?
+  end
 end
