bca04c5abdfadca4270c570f7f221f1664489a59
Naomi Jacobs <naomiajacobs@gmail.com>
2017-08-01 19:47:54 -0700

    Add accurate login state, prevent double rendering at root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  before_action :set_logged_in
+
   def new
-    @logged_in = logged_in?
-    render 'layouts/application.html.erb'
   end
+
+  private
+
+  def set_logged_in
+    @logged_in = logged_in?
+  end
+
 end
